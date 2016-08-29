f60248a31b40408cab28d2905570ec5c4b71a50d
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-29 14:17:03 -0700

    add recipeList container

--- src/containers/recipebox.js
+++ src/containers/recipebox.js
@@ -4,45 +4,32 @@ import { ListGroup, ListGroupItem, Panel, Button, Modals } from 'react-bootstrap
 import MyModal from '../components/mymodal';
 import { bindActionCreators } from 'redux';
 import { deleteRecipe } from '../actions/index';
+import shortid from 'shortid'
+import ModalBox from '../containers/modalbox'
+import RecipeList from '../containers/recipeList'
 
 
 class RecipeBox extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      showModal: false
-    };
-    this.renderRecipeList = this.renderRecipeList.bind(this)
-    this.renderModal = this.renderModal.bind(this)
 
-    this.onClickSubmit = this.onClickSubmit.bind(this);
+    this.renderRecipeList = this.renderRecipeList.bind(this)
     this.handleRecipeNameChange = this.handleRecipeNameChange.bind(this)
     this.handleUserIngredientsChange = this.handleUserIngredientsChange.bind(this)
-    this.toggleModal = this.toggleModal.bind(this);
   }
-  toggleModal(){
-    this.setState({
-        showModal: !this.state.showModal
-    });
-  }
-  onClickSubmit(){
-    this.toggleModal()
-  }
+
   handleRecipeNameChange(event){
     this.setState({recipeName: event.target.value})
   }
   handleUserIngredientsChange(event){
     this.setState({userIngredients: event.target.value})
   }
-  renderModal(list){
-    this.toggleModal()
-    console.log(this.state.showModal)
-  }
-  renderRecipeList(recipeItem,index){
+  renderRecipeList(recipeItem, index){
     const recipe = recipeItem.recipe;
     const ingredients = recipeItem.ingredients;
+    const id = shortid.generate();
     return(
-      <div key={index}>
+      <div key={id}>
         <Panel bsStyle="primary" collapsible header={<h3>{recipe}</h3>}>
           <ListGroup >
             <ListGroupItem  header="Ingredients"></ListGroupItem>
@@ -54,23 +41,14 @@ class RecipeBox extends Component {
                 onClick={() => this.props.deleteRecipe(recipeItem)}
                 bsStyle="danger">Delete
               </Button>
-              <Button
-                onClick={() => this.renderModal(recipeItem)}
-                bsStyle="info">Edit
-              </Button>
+              <RecipeList
+                recipeName={recipe}
+                userIngredients={ingredients}
+                onClickSubmit={this.onClickSubmit}
+              />
             </ListGroupItem>
           </ListGroup>
         </Panel>
-        <MyModal
-          toggleModal={this.toggleModal}
-          showModal={this.state.showModal}
-          recipeName={recipe}
-          userIngredients={ingredients}
-          handleRecipeNameChange={this.handleRecipeNameChange}
-          handleUserIngredientsChange={this.handleUserIngredientsChange}
-          onClickSubmit={this.onClickSubmit}
-          modalTextTitle={'Edit Recipe'}
-        />
       </div>
     )
   }
