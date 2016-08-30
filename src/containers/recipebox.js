0b8810522ea89fa0906c31003ac042e819e2239c
Kenzo Mendoza <kenzom954@gmail.com>
2016-08-30 11:58:47 -0700

    Incorporate the new container, recipe_list

--- src/containers/recipebox.js
+++ src/containers/recipebox.js
@@ -1,60 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ListGroup, ListGroupItem, Panel, Button, Modals } from 'react-bootstrap';
-import { bindActionCreators } from 'redux';
-import { deleteRecipe, editRecipe } from '../actions/index';
-import shortid from 'shortid'
-import Edit from '../containers/edit'
-
+import RecipeList from '../containers/recipe_list'
 
 class RecipeBox extends Component {
   constructor(props){
     super(props);
-    this.renderRecipeList = this.renderRecipeList.bind(this)
-    this.handleRecipeNameChange = this.handleRecipeNameChange.bind(this)
-    this.handleUserIngredientsChange = this.handleUserIngredientsChange.bind(this)
-  }
-
-  handleRecipeNameChange(event){
-    this.setState({recipeName: event.target.value})
-  }
-  handleUserIngredientsChange(event){
-    this.setState({userIngredients: event.target.value})
-  }
-  renderRecipeList(recipeItem, index){
-    const recipe = recipeItem.recipe;
-    const ingredients = recipeItem.ingredients;
-    const recipeID = recipeItem.id;
-    const id = shortid.generate();
-    return(
-      <div key={id}>
-        <Panel bsStyle="primary" collapsible header={<h3>{recipe}</h3>}>
-          <ListGroup >
-            <ListGroupItem  header="Ingredients"></ListGroupItem>
-            {ingredients.map(function(ingredient,index){
-              return <ListGroupItem key={index}>{ingredient}</ListGroupItem>;
-            })}
-            <ListGroupItem>
-              <Button
-                onClick={() => this.props.deleteRecipe(recipeItem)}
-                bsStyle="danger">Delete
-              </Button>
-              <Edit
-                recipeName={recipe}
-                userIngredients={ingredients}
-                recipeID={recipeID}
-              />
-            </ListGroupItem>
-          </ListGroup>
-        </Panel>
-      </div>
-    )
   }
   render(){
+    let itemList = [];
+    this.props.recipeList.forEach(function(item){
+      itemList.push(
+        <RecipeList
+          key={item.id}
+          recipeID={item.id}
+          recipe={item.recipe}
+          ingredients={item.ingredients}
+        />)
+    })
     return(
       <div className="container">
         <div className='panel-group'>
-          {this.props.addRecipe.map(this.renderRecipeList)}
+          {itemList}
         </div>
       </div>
     )
@@ -63,12 +30,8 @@ class RecipeBox extends Component {
 
 function mapStateToProps(state) {
   return {
-    addRecipe : state.recipeState
+    recipeList : state.recipeState
   };
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({deleteRecipe : deleteRecipe, editRecipe : editRecipe}, dispatch)
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(RecipeBox);
+export default connect(mapStateToProps)(RecipeBox);
