2a2e42aff35a700755d013542658bbd98587e493
Kenzo Mendoza <kenzom954@gmail.com>
2016-09-02 21:44:16 -0700

    update links

--- src/components/header.js
+++ src/components/header.js
@@ -8,8 +8,9 @@ export default class Header extends Component {
         <div className="nav-wrapper container">
           <a href="#" className="brand-logo">FCC Recipe Box</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="#">FreeCodeCamp</a></li>
-            <li><a href="#">About</a></li>
+            <li><a href="https://www.freecodecamp.com">FreeCodeCamp</a></li>
+            <li><a href="https://github.com/Neotriz/RecipeBox">Github</a></li >
+            <li><a href="#">About</a></li >
           </ul>
         </div>
       </nav>
